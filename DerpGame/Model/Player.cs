e3a4694a474b73ae2455521bb4af9367e1b7dd0e
GAK00 <geran.kunz@gmail.com>
2017-05-04 08:50:51 -0600

    made player

---
+++ DerpGame/Model/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace DerpGame.Model
+{
+	public class Player
+	{
+		public Player()
+		{
+		}
+		public void Initialize() 
+		{
+		}
+		public void Update() 
+		{
+		}
+		public void Draw() 
+		{
+		}
+	}
+}
